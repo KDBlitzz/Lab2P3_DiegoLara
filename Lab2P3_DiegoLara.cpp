1c810f72a73f911e2b34831bc8fde8342ae11260
Diego Lara <diegoalara2005@gmail.com>
2024-01-26 17:28:24 -0600

    Cuarto

--- Lab2P3_DiegoLara.cpp
+++ Lab2P3_DiegoLara.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include<ctime>
 #include<time.h>
 #include<stdlib.h>
+#include <locale>
 int generarNumRandom(int minimo, int maximo) {
 	int random;
 	random = 0;
@@ -49,7 +50,36 @@ void ejercicio_1() {
 	cout << "]" << endl;
 	cout << "La desviación estándar es: " << desviacionTipica << endl;
 }
+void ejercicio_2() {
+	int arreglo[10] = {6,-6,9,1,-8,2,-1,0,10,7};
+	int contador = 1;
+	int temporal = 0;
+		for (int i = 1; i <= 10; i++)
+		{
+			if (contador == arreglo[i])
+			{
+				contador++;
+				i = 0;
+			}
+			else {
+				if (temporal > contador)
+				{
+					temporal = contador;
+				}
+				
+			}
+		}
+	
+}
+void ejercicio_3() {
+	int arreglo[7] = { 1,2,3,4,5,6,7 };
 
+}
+void swap(int n, int m) {
+}
+void recursiva() {
+
+}
 void menu() {
 	bool menu = true;
 	int op = 0;
@@ -77,6 +107,7 @@ void menu() {
 }
 int main()
 {
+	setlocale(LC_ALL, "spanish");
 	srand(time(NULL));
 	menu();
 }
